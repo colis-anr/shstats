2d01fff6d0249fb4c4c22ed15c6186797ba4b316
Ralf Treinen <treinen@irif.fr>
2018-05-25 11:27:41 +0200

    shstats.ml: suppress unused Opens

--- src/shstats.ml
+++ src/shstats.ml
@@ -1,23 +1,14 @@
 (** Compute some statistics about a CST. *)
 
-open Commands
-open Options
-
-open CommandAnalyzer
-open VariableAnalyzer
-open StructuresAnalyzer
-open MiscAnalyzer
-open FunctionsAnalyzer
-open IdentityAnalyzer
-
 let process (_, file) =
   let cin = open_in file in
-  let (filename, csts) : string * Libmorbig.CST.complete_command list = input_value cin in
+  let (filename, csts) : string * Libmorbig.CST.complete_command list =
+    input_value cin in
   Analyzer.process_script filename csts;
   close_in cin
 
 let main =
   Options.parse_command_line (Analyzer.options ());
   Commands.load_commands_specification ();
-  List.iter process (files ());
+  List.iter process (Options.files ());
   Analyzer.output_report ()
