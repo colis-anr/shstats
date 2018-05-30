1f3c300197a9323b2de3e480189ede6aec3ce082
Nicolas Jeannerod <nicolas.jeannerod@irif.fr>
2018-05-30 17:32:25 +0200

    add ability to have a link between any two reports

--- src/report.ml
+++ src/report.ml
@@ -44,11 +44,16 @@ let create ?(path=[]) title =
   let subreports = Hashtbl.create 8 in
   { title ; path ; buffer ; formatter ; subreports }
 
+let safe_report_name name =
+  let slug = slug ~limit:16 name in
+  let hash = hash name in
+  if slug = "" then hash else slug ^ "." ^ hash
+
 let create_subreport report ?(title="") name =
   if Hashtbl.mem report.subreports name then
     raise (Invalid_argument "add_subreport")
   else
-    let subreport = create ~path:(name :: report.path) (if title = "" then name else title) in
+    let subreport = create ~path:((safe_report_name name) :: report.path) (if title = "" then name else title) in
     Hashtbl.add report.subreports name subreport;
     subreport
 
@@ -66,11 +71,6 @@ let add report = Format.fprintf report.formatter
 
 
 
-
-let safe_report_name name =
-  let slug = slug ~limit:16 name in
-  let hash = hash name in
-  if slug = "" then hash else slug ^ "." ^ hash
 
 let safe_source_path source =
   let target =
@@ -93,34 +93,34 @@ let link = Format.sprintf "[[%s:%s][%s]]"
 let link_to_file target text =
   link "file" target text
 
+let path_to_root report =
+  let path = List.map (fun _ -> "..") report.path in
+  let path = if has_subreports report then path else List.tl path in
+  "./" ^ String.concat "/" path
+
+let path_from_root report =
+  let path = List.rev report.path in
+  let path = "./" ^ String.concat "/" path in
+  if has_subreports report
+  then path ^ "/index.org"
+  else path ^ ".org"
+
+let link_to_report (source : t) (target : t) text =
+  link_to_file
+    (Filename.concat (path_to_root source) (path_from_root target))
+    text
+
 let link_to_subreport report ?(text="") subreport_name =
   try
     let subreport = Hashtbl.find report.subreports subreport_name in
-    link_to_file
-      (
-        if Hashtbl.length subreport.subreports = 0 then
-          (safe_report_name subreport_name) ^ ".org"
-        else
-          Filename.concat (safe_report_name subreport_name) "index.org"
-      )
-      (
-        if text = "" then
-          subreport_name
-        else
-          text
-      )
+    link_to_report report subreport (if text = "" then subreport_name else text)
   with
-    Not_found -> if text = "" then subreport_name else text
+    Not_found -> (if text = "" then subreport_name else text)
 
 let link_to_source report ?(text="") source =
   let path =
     Filename.concat
-      ("./" ^ String.concat "/" (
-                  let path = List.map (fun _ -> "..") report.path in
-                  if has_subreports report
-                  then path
-                  else List.tl path
-      ))
+      (path_to_root report)
       (Filename.concat "_sources" (safe_source_path source))
   in
   link_to_file path (if text = "" then source else text)
