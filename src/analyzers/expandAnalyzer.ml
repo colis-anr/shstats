d1c78e26b0c4b32d39ae829553e6ac5102fb5ed6
Ralf Treinen <treinen@irif.fr>
2018-07-10 17:58:15 +0200

    expand analyzer: handln of commands that might be function calls

--- src/analyzers/expandAnalyzer.ml
+++ src/analyzers/expandAnalyzer.ml
@@ -43,9 +43,9 @@ let assigned_by_word w =
      in collect_from 0
 ;;
 
-(* check whether [s] is (possibly) the name of a variable *)
-let is_variable s =
-  String.length s > 1 && String.get s 0 = '$'
+(* check whether [s] is possibly expandable by parameter or sub-shell
+expansion *)
+let is_expandable s = String.contains s '$' || String.contains s '`'
 
 module Mon = struct
 (* a value of type [t] is an over-approximation of the effect of executing a
@@ -62,8 +62,6 @@ module Mon = struct
     }
    and fenv = (string*t) list
   let zero = {vars=StringSet.empty; fncts= []}
-  let from_var x = {vars=StringSet.singleton x; fncts=[]}
-  let from_varlist l =  {vars=StringSet.of_list l; fncts=[]}
   (* the [plus] of two values of type [t] is defined as the respective
       unions of their [vars] and their [fncts]. If a function name
       is defined in both then we recursively compute the [plus] of the
@@ -76,6 +74,11 @@ module Mon = struct
        in f1o@f2o@
             (List.map (function (f,i) -> (f, plus i (List.assoc f f21))) f12)
     }
+  let from_var x = {vars=StringSet.singleton x; fncts=[]}
+  let from_varlist l =  {vars=StringSet.of_list l; fncts=[]}
+  let merge_functions effect fncts =
+    List.fold_left (fun m (f,i) -> plus m i) effect fncts
+      
 end
            
 module Self : Analyzer.S = struct
@@ -96,19 +99,47 @@ module Self : Analyzer.S = struct
             self#plus
               (Mon.from_var (Libmorbig.CSTHelpers.unName name))
               (self#visit_word fcts word)
-          method! visit_simple_command fcts cst = match cst with
+          method! visit_simple_command fcts = function
             | SimpleCommand_CmdPrefix_CmdWord_CmdSuffix (pre',cw',suf') ->
-               if List.mem_assoc (unCmdWord' cw') fcts
-                  || is_special_builtin (unCmdWord' cw')
-               then self#plus
-                      (self#visit_cmd_suffix' fcts suf')
-                      (self#visit_cmd_prefix' fcts pre')
-               else self#visit_cmd_suffix' fcts suf'
+               let cmd = UnQuote.on_string (unCmdWord' cw')
+               and prefix_effect = self#visit_cmd_prefix' fcts pre'
+               and suffix_effect = self#visit_cmd_suffix' fcts suf' 
+               in let presuf_effect = self#plus prefix_effect suffix_effect 
+               in
+               if is_expandable cmd
+               then
+                 (* [cmd] might be expanded to anything: to a function or
+                     special builtin, so we have to take the effect of the
+                     prefix into account, and it might even be one of the
+                     currently visible functions. *)
+                 Mon.merge_functions presuf_effect fcts
+               else if List.mem_assoc cmd fcts
+               then
+                 (* call to a known function *)
+                 self#plus presuf_effect (List.assoc cmd fcts)
+               else if is_special_builtin cmd
+               then presuf_effect
+               else (* creation of a process *)
+                 suffix_effect
             | SimpleCommand_CmdPrefix_CmdWord (pre',cw') ->
-               if List.mem_assoc (unCmdWord' cw') fcts
-                  || is_special_builtin (unCmdWord' cw')
-               then self#visit_cmd_prefix' fcts pre'
-               else self#zero
+               let cmd = UnQuote.on_string (unCmdWord' cw')
+               and prefix_effect = self#visit_cmd_prefix' fcts pre'
+               in
+               if is_expandable cmd
+               then
+                 (* [cmd] might be expanded to anything: to a function or
+                     special buildin, so we have the effect of the prefix
+                     into account, and it might even be one of the currently
+                     visible functions. *)
+                 Mon.merge_functions prefix_effect fcts
+               else if List.mem_assoc cmd fcts
+               then
+                 (* call to a known function *)
+                 self#plus prefix_effect (List.assoc cmd fcts)
+               else if is_special_builtin cmd
+               then prefix_effect
+               else (* creation of a process *)
+                 self#zero
             | SimpleCommand_CmdPrefix pre' ->
                self#visit_cmd_prefix' fcts pre'
             | SimpleCommand_CmdName_CmdSuffix (nam',suf') ->
@@ -119,7 +150,7 @@ module Self : Analyzer.S = struct
            
         end
       in
-      (effect_collector#visit_complete_command_list [] cst).vars
+      (effect_collector#visit_complete_command_list [] cst).Mon.vars
 
     in
     let cout = open_out (filename^".vars")
