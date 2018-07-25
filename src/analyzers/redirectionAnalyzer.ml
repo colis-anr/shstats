6ca9542b2acad948e1f8eb6ac530e1b516449b89
Nicolas Jeannerod <nicolas.jeannerod@irif.fr>
2018-07-25 12:34:00 +0200

    redirection: add abstract representation of redirections

--- src/analyzers/redirectionAnalyzer.ml
+++ src/analyzers/redirectionAnalyzer.ml
@@ -19,15 +19,165 @@ type location_simple =
 type location =
   Assignment | Compound | Function | Simple of location_simple
 
+(* ============== [ Abstract representation of redirections ] =============== *)
+
+let is_expandable word =
+  String.index_opt word '$' <> None
+
+let is_quoted word =
+  String.index_opt word '\'' <> None
+  || String.index_opt word '"' <> None
+  || String.index_opt word '\\' <> None
+
+type a_descr =
+  | None
+  | Int of int
+  | Expandable
+
+let pp_a_descr fmt = function
+  | None -> ()
+  | Int i -> Format.pp_print_int fmt i
+  | Expandable -> Format.pp_print_string fmt "{expandable}"
+
+let a_descr_of_io_number = function
+  | IONumber n ->
+     try Int (int_of_string n)
+     with _ -> Expandable
+
+let a_descr_of_filename = function
+  | Filename_Word {value=Word (word, _);_} ->
+     try Int (int_of_string word)
+     with _ -> Expandable
+
+type a_file =
+  | DevNull
+  | Constant
+  | Expandable
+
+let pp_a_file fmt a_file =
+  Format.pp_print_string
+    fmt
+    (match a_file with
+     | DevNull -> "/dev/null"
+     | Constant -> "{constant}"
+     | Expandable -> "{expandable}")
+
+let a_file_of_filename = function
+  | Filename_Word {value=Word (word, _);_} ->
+     if word = "/dev/null" then
+       DevNull
+     else if is_expandable word then
+       Expandable
+     else
+       Constant
+
+type a_delim =
+  | Unquoted
+  | Quoted
+  | Expandable
+
+let pp_a_delim fmt a_delim =
+  Format.pp_print_string
+    fmt
+    (match a_delim with
+     | Unquoted -> "{unquoted}"
+     | Quoted -> "{quoted}"
+     | Expandable -> "{expandable}")
+
+let a_delim_of_here_end = function
+  | HereEnd_Word {value=Word (word, _);_} ->
+     if is_expandable word then
+       Expandable
+     else if is_quoted word then
+       Quoted
+     else
+       Unquoted
+
+type a_redirection =
+  | Output of a_descr * a_file
+  | OutputClobber of a_descr * a_file
+  | OutputAppend of a_descr * a_file
+  | OutputDuplicate of a_descr * a_descr
+  | Input of a_descr * a_file
+  | InputDuplicate of a_descr * a_descr
+  | InputOutput of a_descr * a_file
+  | Here of a_descr * bool * a_delim
+
+let pp_a_redirection fmt =
+  let p = Format.fprintf in
+  function
+  | Output (a_descr, a_file) ->
+     p fmt "%a>%a" pp_a_descr a_descr pp_a_file a_file
+  | OutputClobber (a_descr, a_file) ->
+     p fmt "%a>|%a" pp_a_descr a_descr pp_a_file a_file
+  | OutputAppend (a_descr, a_file) ->
+     p fmt "%a>>%a" pp_a_descr a_descr pp_a_file a_file
+  | OutputDuplicate (a_descr_1, a_descr_2) ->
+     p fmt "%a>&%a" pp_a_descr a_descr_1 pp_a_descr a_descr_2
+  | Input (a_descr, a_file) ->
+     p fmt "%a<%a" pp_a_descr a_descr pp_a_file a_file
+  | InputDuplicate (a_descr_1, a_descr_2) ->
+     p fmt "%a<&%a" pp_a_descr a_descr_1 pp_a_descr a_descr_2
+  | InputOutput (a_descr, a_file) ->
+     p fmt "%a<>%a" pp_a_descr a_descr pp_a_file a_file
+  | Here (a_descr, strip, a_delim) ->
+     p fmt "%a<<%s%a" pp_a_descr a_descr (if strip then "-" else "") pp_a_delim a_delim
+
+let a_redirection_of_io_file a_descr = function
+  | IoFile_Less_FileName filename' ->
+     Input (a_descr, a_file_of_filename filename'.value)
+  | IoFile_LessAnd_FileName filename' ->
+     InputDuplicate (a_descr, a_descr_of_filename filename'.value)
+  | IoFile_Great_FileName filename' ->
+     Output (a_descr, a_file_of_filename filename'.value)
+  | IoFile_GreatAnd_FileName filename' ->
+     OutputDuplicate (a_descr, a_descr_of_filename filename'.value)
+  | IoFile_DGreat_FileName filename' ->
+     OutputAppend (a_descr, a_file_of_filename filename'.value)
+  | IoFile_LessGreat_FileName filename' ->
+     InputOutput (a_descr, a_file_of_filename filename'.value)
+  | IoFile_Clobber_FileName filename' ->
+     OutputClobber (a_descr, a_file_of_filename filename'.value)
+
+let a_redirection_of_io_here a_descr = function
+  | IoHere_DLess_HereEnd (here_end', _) ->
+     Here (a_descr, false, a_delim_of_here_end here_end'.value)
+  | IoHere_DLessDash_HereEnd (here_end', _) ->
+     Here (a_descr, true, a_delim_of_here_end here_end'.value)
+
+let a_redirection_of_io_redirect = function
+  | IoRedirect_IoFile io_file' ->
+     a_redirection_of_io_file None io_file'.value
+  | IoRedirect_IoNumber_IoFile (io_number, io_file') ->
+     a_redirection_of_io_file (a_descr_of_io_number io_number) io_file'.value
+  | IoRedirect_IoHere io_here' ->
+     a_redirection_of_io_here None io_here'.value
+  | IoRedirect_IoNumber_IoHere (io_number, io_here') ->
+     a_redirection_of_io_here (a_descr_of_io_number io_number) io_here'.value
+
+(* =============================== [ Result ] =============================== *)
+    
 type result =
   { filename : string ;
     location : location ;
-    content : io_redirect' list }
+    concrete : io_redirect' list ;
+    abstract : a_redirection list }
 
+let make_result filename location concrete =
+  assert (concrete <> []);
+  { filename ; location ; concrete ;
+    abstract =
+      List.map
+        (fun io_redirect' -> a_redirection_of_io_redirect io_redirect'.value)
+        concrete }
+
+let make_one_result filename location concrete =
+  [make_result filename location concrete]
+  
 let results : result list ref = ref []
 
 (* ===================== [ Helpers about redirections ] ===================== *)
-                              
+
 let cmd_prefix_to_io_redirect_list cmd_prefix' =
   let rec aux acc = function
     | CmdPrefix_IoRedirect io_redirect' ->
@@ -63,41 +213,8 @@ let redirect_list_to_io_redirect_list redirect_list' =
   in
   aux [] redirect_list'.value
 
-type kind =
-  | Input
-  | Output
-  | OutputClobber
-  | OutputAppend
-  | DuplicateInput
-  | DuplicateOutput
-  | InputOutput
-  | Here
-  | HereStripped
-  
-let io_redirect_to_kind (io_redirect' : io_redirect') : kind =
-  match io_redirect'.value with
-  | IoRedirect_IoFile io_file'
-  | IoRedirect_IoNumber_IoFile (_, io_file') ->
-     (
-       match io_file'.value with
-       | IoFile_Less_FileName _ -> Input
-       | IoFile_LessAnd_FileName _ -> DuplicateInput
-       | IoFile_Great_FileName _ -> Output
-       | IoFile_GreatAnd_FileName _ -> DuplicateOutput
-       | IoFile_DGreat_FileName _ -> OutputAppend
-       | IoFile_LessGreat_FileName _ -> InputOutput
-       | IoFile_Clobber_FileName _ -> OutputClobber
-     )
-  | IoRedirect_IoHere io_here'
-  | IoRedirect_IoNumber_IoHere (_, io_here') ->
-     (
-       match io_here'.value with
-       | IoHere_DLess_HereEnd _ -> Here
-       | IoHere_DLessDash_HereEnd _ -> HereStripped
-     )
-  
 (* =========================== [ Process Script ] =========================== *)
-  
+
 let process_script filename csts =
   let visitor = object (self)
     inherit [_] reduce as super
@@ -110,9 +227,8 @@ let process_script filename csts =
         (match command with
          | Command_CompoundCommand_RedirectList (_, redirect_list') ->
             (
-              let content = redirect_list_to_io_redirect_list redirect_list' in
-              assert (content <> []);
-              [{ filename ; location = Compound ; content }]
+              redirect_list_to_io_redirect_list redirect_list'
+              |> make_one_result filename Compound
             )
          | _ -> self#zero)
         (super#visit_command () command)
@@ -122,9 +238,8 @@ let process_script filename csts =
         (match function_body with
          | FunctionBody_CompoundCommand_RedirectList (_, redirect_list') ->
             (
-              let content = redirect_list_to_io_redirect_list redirect_list' in
-              assert (content <> []);
-              [{ filename ; location = Function ; content }]
+              redirect_list_to_io_redirect_list redirect_list'
+              |> make_one_result filename Function
             )
          | _ -> self#zero)
         (super#visit_function_body () function_body)
@@ -138,30 +253,30 @@ let process_script filename csts =
               let in_suffix = cmd_suffix_to_io_redirect_list cmd_suffix' in
               match in_prefix, in_suffix with
               | [], [] -> []
-              | _, [] -> [{ filename ; location = Simple Prefix ; content = in_prefix }]
-              | [], _ -> [{ filename ; location = Simple Suffix ; content = in_suffix }]
-              | _, _ -> [{ filename ; location = Simple Both ; content = in_prefix @ in_suffix }]
+              | _, [] -> make_one_result filename (Simple Prefix) in_prefix
+              | [], _ -> make_one_result filename (Simple Suffix) in_suffix
+              | _, _ -> make_one_result filename (Simple Both) (in_prefix @ in_suffix)
             )
          | SimpleCommand_CmdPrefix_CmdWord (cmd_prefix', _) ->
             (
               let content = cmd_prefix_to_io_redirect_list cmd_prefix' in
               if content = []
               then []
-              else [{ filename ; location = Simple Prefix ; content }]
+              else make_one_result filename (Simple Prefix) content
             )
          | SimpleCommand_CmdPrefix cmd_prefix' ->
             (
               let content = cmd_prefix_to_io_redirect_list cmd_prefix' in
               if content = []
               then []
-              else [{ filename ; location = Assignment ; content }]
+              else make_one_result filename Assignment content
             )
          | SimpleCommand_CmdName_CmdSuffix (_, cmd_suffix') ->
             (
               let content = cmd_suffix_to_io_redirect_list cmd_suffix' in
               if content = []
               then []
-              else [{ filename ; location = Simple Suffix ; content }]
+              else make_one_result filename (Simple Suffix) content
             )
          | _ -> self#zero)
         (super#visit_simple_command () simple_command)
@@ -230,12 +345,12 @@ let output_report report =
       result.content
     <> None
   in
-  
+
   Report.add report "** =<= (input)\n";
   !results
   |> List.filter (involves_kind Input)
   |> output_file_list report;
-  
+
   Report.add report "** =>= (output)\n";
   !results
   |> List.filter (involves_kind Output)
