37895be1bb85904784335d39eea31b38cf7d69d8
Ralf Treinen <treinen@irif.fr>
2019-03-14 18:02:14 +0100

    testAnalyzer: use parser for test expressions from morsmall

--- src/analyzers/testAnalyzer.ml
+++ src/analyzers/testAnalyzer.ml
@@ -10,6 +10,7 @@ open ExtPervasives
 open Morbig.CST
 open Options
 open Messages
+open Morsmall.Utilities.TestParser
 
 module Name =
   struct
@@ -55,145 +56,6 @@ let extract_arguments = function
   | SimpleCommand_CmdName _
     -> []
 
-(* tokens for arguments of the test command *)
-type token =
-  | UnOp of string    (* unary operators -e, -f, etc. *) 
-  | BinOp of string   (* binary operators -eq, =, etc. *)
-  | AndOp             (* -a *)
-  | OrOp              (* -o *)
-  | Not               (* ! *)
-  | ParL              (* ( *)
-  | ParR              (* ) *)
-  | BracketR          (* ] *)
-  | String of string  (* all the rest *)
-  | EOF
-
-let to_token s = match s with
-  (* file existence and type *)
-  | "-e" | "-d" | "-f" | "-b" | "-c" | "-h" | "-L" | "-p" | "-S" -> UnOp s
-  (* file attributes *)
-  | "-g" | "-u" | "-s" | "-r" | "-w" | "-x" -> UnOp s
-  (* GNU extension on files *)
-  | "-G" | "-O" | "-k" -> UnOp s
-  (* GNU extension on files *)
-  | "-nt" | "-ot" | "-ef" -> BinOp s
-  (* unary operators on strings *)
-  | "-n" | "-z" -> UnOp s
-  (* binary operators on strings *)
-  | "=" | "!=" -> BinOp s
-  (* binary operators on integers *)
-  | "-eq" | "-ne" | "-gt" | "-ge" | "-lt" | "-le" -> BinOp s
-  (* unary operator on file descriptor *)
-  | "-t" -> UnOp s
-  | "-a" -> AndOp
-  | "-o" -> OrOp
-  | "("  -> ParL
-  | ")"  -> ParR
-  | "]"  -> BracketR
-  | "!"  -> Not
-  | _    -> String s
-
-(* abstract syntax of test expressions *)
-type test_expression =
-  | Andtest of test_expression * test_expression
-  | Ortest of test_expression * test_expression
-  | Onetest of test_literal
-and test_literal =
-  | Postest of test_atom
-  | Negtest of test_atom
-and test_atom =
-  | Bintest of string * string * string  (* op arg_left arg_right *)
-  | Unitest of string * string           (* op arg *)
-  | Contest of string                    (* arg *)
-  | Partest of test_expression           (* ( expression ) *)
-
-let rec has_bool_combinators = function
-  | Andtest (_,_) | Ortest (_,_) -> true
-  | Onetest l -> has_bool_lit l
-and has_bool_lit = function
-  | Postest a | Negtest a -> has_bool_atom a
-and has_bool_atom = function
-  | Partest e -> has_bool_combinators e
-  | _ -> false
-
-exception Parse_error
-let parse is_bracket tokens =
-  let tokenbuf = ref tokens in
-  let lookup () = match !tokenbuf with
-    | h::r -> h
-    | [] -> EOF
-  and pop () = match !tokenbuf with
-    | h::r -> tokenbuf := r
-    | [] -> assert false
-  in
-  let rec parse_S () =
-    let exp = parse_disj () in
-    if is_bracket then
-      if lookup () = BracketR
-      then pop ()
-      else raise Parse_error;
-    if lookup () = EOF
-    then exp
-    else raise Parse_error
-  and parse_disj () =
-    let head = parse_conj () in
-    match parse_disj' () with
-    | None -> head
-    | Some rest -> Ortest (head,rest)
-  and parse_disj' () =
-    match lookup () with
-    | EOF | BracketR | ParR -> None
-    | OrOp -> pop (); Some (parse_disj ())
-    | _ -> raise Parse_error
-  and parse_conj () =
-    let head = Onetest (parse_literal ()) in
-    match parse_conj' () with
-    | None -> head
-    | Some rest ->  Andtest (head, rest)
-  and parse_conj' () =
-    match lookup () with
-    | OrOp | EOF | BracketR | ParR -> None
-    | AndOp -> pop (); Some (parse_conj ())
-    | _ -> raise Parse_error
-  and parse_literal () =
-    match lookup () with
-    | Not -> pop (); Negtest (parse_atom ())
-    | UnOp _ | ParL | String _ -> Postest (parse_atom ())
-    | _ -> raise Parse_error
-  and parse_atom () =
-    match lookup () with
-    | UnOp op -> pop ();
-                 begin
-                 match lookup () with
-                 | String s -> pop (); Unitest (op,s)
-                 | _ -> raise Parse_error
-                 end
-    | ParL -> pop ();
-              begin
-              let exp = parse_disj () in
-              match lookup () with
-              | ParR -> pop (); Partest exp
-              | _ -> raise Parse_error
-              end
-    | String s -> pop ();
-                  begin
-                  match parse_atom' () with
-                  | None -> Contest s
-                  | Some (binop,rightarg) -> Bintest (binop,s,rightarg)
-                  end
-    | _ -> raise Parse_error
-  and parse_atom' () =
-    match lookup () with
-    | AndOp | OrOp | EOF | BracketR -> None
-    | BinOp binop -> pop ();
-                     begin
-                     match lookup () with
-                     | String rightarg | UnOp rightarg | BinOp rightarg
-                       -> pop (); Some (binop,rightarg)
-                     | _ -> raise Parse_error
-                     end
-    | _ -> raise Parse_error
-  in parse_S ()       
 
 class stringcounter = object (self)
   val counters : (string,int) Hashtbl.t = Hashtbl.create 8
@@ -266,14 +128,10 @@ let in_fundef = ref false
 let process_script filename csts =
 
   let rec process_expr = function
-    | Andtest (e1,e2) -> process_expr e1; process_expr e2
-    | Ortest (e1,e2) -> process_expr e1; process_expr e2
-    | Onetest l -> process_lit l
-  and process_lit = function
-    | Postest a -> process_atom a
-    | Negtest a -> process_atom a
-  and process_atom = function
-    | Bintest (op,left,right) -> begin
+    | And (e1,e2) -> process_expr e1; process_expr e2
+    | Or  (e1,e2) -> process_expr e1; process_expr e2
+    | Not e       -> process_expr e
+    | Binary (op,left,right) -> begin
         count_binops#incr(op);
         if not (!in_fundef) &&
              (op = "=" || op = "!=") && (left="$1"||right="$1")
@@ -286,26 +144,28 @@ let process_script filename csts =
                    scripts_with_strange_dollarone#add otherarg filename
                end
       end
-    | Unitest (op,_) -> count_uniops#incr(op)
-    | Contest _ -> incr count_contest
-    | Partest e -> process_expr e
+    | Unary (op,_) -> count_uniops#incr(op)
+    | Single _ -> incr count_contest
   in
-  
+  let has_bool_combinators = function
+    | And _ | Or _  -> true
+    | _ -> false
+  in
   let register_test filename invocation arguments =
     let arguments_unquoted = List.map Morbig.remove_quotes arguments
-    and is_bracket = (invocation = "[" )
+    and bracket = (invocation = "[" )
     in
     incr count_testinvocations;
     try
-      let ast = parse is_bracket (List.map to_token arguments_unquoted)
-      in
-      process_expr ast;
-      if has_bool_combinators ast
-      then begin
-          scripts_with_complex_tests :=
-            NameSet.add filename (!scripts_with_complex_tests);
-          incr count_complex_tests
-        end
+      match parse ~bracket arguments_unquoted with
+      | None -> ()
+      | Some ast -> process_expr ast;
+                    if has_bool_combinators ast
+                    then begin
+                        scripts_with_complex_tests :=
+                          NameSet.add filename (!scripts_with_complex_tests);
+                        incr count_complex_tests
+                      end
     with
       Parse_error ->
       parsing_errors := (filename, invocation, arguments) :: !parsing_errors
